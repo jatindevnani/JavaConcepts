81ce80cbe9ca34fb1215b194322221986d05e49b
jatindevnani <81103012+jatindevnani@users.noreply.github.com>
2022-05-12 21:35:25 +0530

    Interface vs abstract

---
+++ src/_4_Interview_Questions/_15_Interface_vs_Abstract_class.java
@@ -0,0 +1,16 @@
+package _4_Interview_Questions;
+
+public class _15_Interface_vs_Abstract_class {
+    //Interface provide 100% abstraction while abstract classes provide 0-100% abstraction
+    //Interfaces can now have default methods as well
+    //Interfaces can also have static methods
+    //Interfaces cannot have concrete methods
+    //Interface can have variables, but they must be "public, static and final"
+    //An interface cannot have a constructor
+    //It can have multiple inheritances
+
+    //Abstract classes can have instance variables and concrete methods as well.
+    //Abstract classes can have a constructor as well
+    //Abstract classes variables can be non-static and non-final as well
+    //Cannot have multiple inheritances. (A class can only inherit one abstract class)
+}
