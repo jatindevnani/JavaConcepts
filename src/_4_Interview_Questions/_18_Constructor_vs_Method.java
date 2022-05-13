305a2a6a92550d212d43c5164e877fb66af9a4a5
jatindevnani <81103012+jatindevnani@users.noreply.github.com>
2022-05-13 13:52:01 +0530

    Constructor vs method

---
+++ src/_4_Interview_Questions/_18_Constructor_vs_Method.java
@@ -0,0 +1,9 @@
+package _4_Interview_Questions;
+
+public class _18_Constructor_vs_Method {
+    //Constructor is required to initialize an object of a class
+    //Method is used to exhibit some functionality of an object
+
+    //Constructors are invoked implicitly
+    //Methods are invoked explicitly
+}
