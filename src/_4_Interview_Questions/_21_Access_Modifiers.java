00e97a4552fa4743308bf61d58803c32c98f97d3
jatindevnani <81103012+jatindevnani@users.noreply.github.com>
2022-05-13 17:36:44 +0530

    Access and non  access modifiers

--- src/_4_Interview_Questions/_21_Access_Modifiers.java
+++
@@ -1,4 +0,0 @@
-package _4_Interview_Questions;
-
-public class _21_Access_Modifiers {
-}
---
+++ src/_4_Interview_Questions/_21_Access_Modifiers_and_Non_Access_Modifiers.java
@@ -0,0 +1,23 @@
+package _4_Interview_Questions;
+
+public class _21_Access_Modifiers_and_Non_Access_Modifiers {
+    /*
+    * Public --> Available to everyone, inside the class, outside the class, inside the package, outside the package
+    * Default --> Available inside the package and to the subclasses that are outside the package
+    * Protected --> Only available inside the package*
+    * Private --> Only available inside the class/
+    *
+    * If you are overriding any method, overridden method (i.e. declared in subclass) must not be more restrictive.
+    *
+    * Java provides a number of non-access modifiers to achieve many other functionalities.
+
+The static modifier for creating class methods and variables.
+
+The final modifier for finalizing the implementations of classes, methods, and variables.
+
+The abstract modifier for creating abstract classes and methods.
+
+The synchronized and volatile modifiers, which are used for threads.
+    */
+}
+
