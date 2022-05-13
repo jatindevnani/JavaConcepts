6b8f0b5e20d8a6c9fbf9b82c3c4a7f65cdc9033b
jatindevnani <81103012+jatindevnani@users.noreply.github.com>
2022-05-13 15:29:40 +0530

    PassByValue vs PassByReference

---
+++ src/_4_Interview_Questions/_19_PassByValue_Or_PassByReference.java
@@ -0,0 +1,14 @@
+package _4_Interview_Questions;
+
+public class _19_PassByValue_Or_PassByReference {
+    //Java always works as Pass by value and there is no such thing as pass by reference
+    //Now what is pass by value you may ask?
+    //Value in java always refers to the primitive value that is stored in the stack memory
+    //For java objects, the stack stores the memory address of the object from heap.
+
+    //When an object is passed to a new method.
+    //The address value is copied into a new variable
+    //and this variable is sent to the method
+
+
+}
